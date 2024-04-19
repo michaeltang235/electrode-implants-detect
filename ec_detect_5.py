b8f9ccc3db67af94f42a1f1abb3f0678204654d3
michaeltang235 <62654864+michaeltang235@users.noreply.github.com>
2024-04-19 16:52:37 -0600

    Add files via upload

--- ec_detect_5.py
+++ ec_detect_5.py
@@ -43,7 +43,7 @@ def min_max_norm(img_input):
     #
     return img_subset_min_max_norm
 
-def select_img(anat_img, elect_ijk_anat, spac, norm_type):
+def select_img(anat_img, elect_ijk_anat, spac):
     #
     img_sel = []
     img_output = []
@@ -64,18 +64,41 @@ def select_img(anat_img, elect_ijk_anat, spac, norm_type):
     #
     img_output = img_sel[:]
     #
-    # conduct normalization of img selected based on norm_type input
-    if norm_type == 2:
-        img_output = mean_zero_norm(img_sel)
-    if norm_type == 3:
-        img_output = standardization(img_sel)
-    if norm_type == 4:
-        img_output = min_max_norm(img_sel)
-    #
     return img_output
 
+# def select_img(anat_img, elect_ijk_anat, spac, norm_type):
+#     #
+#     img_sel = []
+#     img_output = []
+#     #
+#     for i in range(len(elect_ijk_anat)):
+#         # for each electrode contact, get image space coordinates, subtract 1 for 0-based indexing
+#         x_coor = int(elect_ijk_anat[i][0]) - 1
+#         y_coor = int(elect_ijk_anat[i][1]) - 1
+#         z_coor = int(elect_ijk_anat[i][2]) - 1
+#         # specify spacing on each side of electrode contact
+#         #spac = 5
+#         # on x-y plane
+#         # crop out patch of image in every cartesian plane, append to img_sel
+#         img_cropped = np.transpose(anat_img[x_coor - spac:x_coor + spac + 1:1, \
+#                                    y_coor - spac:y_coor + spac + 1:1, z_coor - spac:z_coor + spac + 1:1],
+#                                    axes=[1, 0, 2])
+#         img_sel.append(img_cropped)
+#     #
+#     img_output = img_sel[:]
+#     #
+#     # # conduct normalization of img selected based on norm_type input
+#     # if norm_type == 2:
+#     #     img_output = mean_zero_norm(img_sel)
+#     # if norm_type == 3:
+#     #     img_output = standardization(img_sel)
+#     # if norm_type == 4:
+#     #     img_output = min_max_norm(img_sel)
+#     # #
+#     return img_output
 
-def compute_w_1(ref_img, img_sel):
+
+def compute_w_1(ref_img, img_sel, norm_type):
     #
     # initialize list to store best-aligned image of each electrode contact
     # with reference image
@@ -87,11 +110,43 @@ def compute_w_1(ref_img, img_sel):
     for curr_img in img_sel:
         _, _, _, _, patch_max_ncc = ec_detect.compute_ncc(ref_img, curr_img)
         shifted_img.append(patch_max_ncc)
+
+    # note: patch_mac_ncc has same dimensions as ref_img, while every
+    # item in img_sel is bigger than ref_img, such that the ref_img
+    # can be translated on each img_sel to obtain translation with max
+    # ncc value
     #
-    # compute average shifted images (elementwise average of 3d arrays)
-    w_1 = np.mean(shifted_img, axis = 0)
+    # conduct normalization of shifted image based on norm_type input
+    shifted_img_norm = shifted_img[:]
+    if norm_type == 2:
+        shifted_img_norm = mean_zero_norm(shifted_img)
+    if norm_type == 3:
+        shifted_img_norm = standardization(shifted_img)
+    if norm_type == 4:
+        shifted_img_norm = min_max_norm(shifted_img)
+    #
+    # compute average normalized shifted images (elementwise average of 3d arrays)
+    w_1 = np.mean(shifted_img_norm, axis = 0)
     #
     return w_1
+
+# def compute_w_1(ref_img, img_sel):
+#     #
+#     # initialize list to store best-aligned image of each electrode contact
+#     # with reference image
+#     shifted_img = []
+#     #
+#     # for every image of electrode contact cropped above, use compute_ncc()
+#     # to align the image with reference image, append the aligned image to
+#     # shifted_img
+#     for curr_img in img_sel:
+#         _, _, _, _, patch_max_ncc = ec_detect.compute_ncc(ref_img, curr_img)
+#         shifted_img.append(patch_max_ncc)
+#     #
+#     # compute average shifted images (elementwise average of 3d arrays)
+#     w_1 = np.mean(shifted_img, axis = 0)
+#     #
+#     return w_1
 
 import ec_detect_4
 import ec_detect_4_1
